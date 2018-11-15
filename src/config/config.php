0e428ab1e061c1bcd8e0a1d3ab9c4410a9bc34f7
mrs-joker <44361825+mrs-joker@users.noreply.github.com>
2018-11-15 17:32:53 +0800

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -5,13 +5,16 @@ return [
         'model' => App\Tree::class,
     ],
     'rbac' => [
-        'user' => '\App\User',
-        'role' => '\MrsJoker\Trade\Rbac\Models\RbacRole',
-        'permission' => '\MrsJoker\Trade\Rbac\Models\RbacPermission',
+//        'user' => '\App\User',
+//        'role' => '\MrsJoker\Trade\Rbac\Models\RbacRole',
+//        'permission' => '\MrsJoker\Trade\Rbac\Models\RbacPermission',
+        'cache_prefix' => 'trade_cache_',
+        'user' => \App\User::class,
+        'role' => \MrsJoker\Trade\Rbac\Models\RbacRole::class,
+        'permission' => \MrsJoker\Trade\Rbac\Models\RbacPermission::class,
         'table_role_user' => 'rbac_role_user',
         'user_foreign_pivot_key' => 'user_id',
         'role_related_pivot_key' => 'role_id',
-
         'table_permission_role' => 'rbac_permission_role',
     ],
 
